31024a7d93865050e58afe3b7cf28aa92bd9a791
MattMgr <matteo.mgr6@gmail.com>
2024-03-12 18:08:21 +0100

    es2 es3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const comment = "This website is for losers LOL!";
+/*const comment = "This website is for losers LOL!";
+const vowals = ["a", "e", "i", "o", "u"];
 const removeVowals = (comment) => {
-  const vowals = ["a", "e", "i", "o", "u"];
   let stringArray = comment.split("");
   console.log(stringArray);
   let stringFiltered = stringArray.filter((word) => {
@@ -10,3 +10,33 @@ const removeVowals = (comment) => {
 };
 
 removeVowals(comment);
+
+const str = "subalawe";
+
+const vowalsCounter = (string) => {
+  let strArray = string.split("");
+  let counter = 0;
+  console.log(strArray);
+  strArray.forEach((element) => {
+    if (vowals.includes(element)) {
+      counter++;
+    }
+  });
+  return counter;
+};
+
+console.log(vowalsCounter(str)); */
+
+const str1 = "middle";
+
+const middleWord = (string) => {
+  const i = Math.floor(string.length / 2);
+  //let result = string[i];
+  if (str.length % 2 === 0 && i > 0) {
+    //result = string[i - 1] + result;
+    return string[i - 1] + string[i];
+  } else return string[i];
+  //return result;
+};
+
+console.log(middleWord(str1));
