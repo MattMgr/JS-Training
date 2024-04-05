19ba4a93ce4894a8169884e6da094f8b0d6752fc
MattMgr <matteo.mgr6@gmail.com>
2024-04-05 19:11:41 +0200

    .

--- jsExercises/11.js
+++ jsExercises/11.js
@@ -1,1 +1,8 @@
-function rgbConverter(r, g, b) {}
+function rgbConverter(r, g, b) {
+  const rHex = parseInt(r).toString(16).padStart(2, "0");
+  const gHex = parseInt(g).toString(16).padStart(2, "0");
+  const bHex = parseInt(b).toString(16).padStart(2, "0");
+  return "#" + rHex + gHex + bHex;
+}
+
+console.log(rgbConverter(123, 234, 20));
